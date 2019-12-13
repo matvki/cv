650e40f2cfd1646e6b662ae1bb9338ab75c3f9fd
admin <admin@iMac3.local>
2019-12-13 12:04:37 +0100

    last version cv.html

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,5 +1,4 @@
 window.onscroll = function() {myFunction()};
-
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
 
@@ -10,21 +9,31 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
 function adresse() {
     if (document.getElementById("adresse").style.visibility== "visible")
     document.getElementById("adresse").style.visibility= "hidden";
     else
     document.getElementById("adresse").style.visibility= "visible";
 }
+
 function email() {
     if (document.getElementById("email").style.visibility== "visible")
     document.getElementById("email").style.visibility= "hidden";
     else
     document.getElementById("email").style.visibility= "visible";
 }
+
 function github() {
     if (document.getElementById("github").style.visibility== "visible")
     document.getElementById("github").style.visibility= "hidden";
     else
     document.getElementById("github").style.visibility= "visible";
+}
+
+function a2015() {
+    if (document.getElementById("a2015").style.visibility== "visible")
+    document.getElementById("a2015").style.visibility= "hidden";
+    else
+    document.getElementById("a2015").style.visibility= "visible";
 }
